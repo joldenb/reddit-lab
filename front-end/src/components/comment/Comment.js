20b4619ecd858723753f12c2447fb833f27d3b9b
Alex White <awhit012@gmail.com>
2018-01-31 12:07:09 -0600

    resets to starter code

--- front-end/src/components/comment/Comment.js
+++ front-end/src/components/comment/Comment.js
@@ -1,14 +1,1 @@
-import React, { Component } from 'react';
-import './Comment.css';
-
-class Comment extends Component {
-  render() {
-    return (
-      <p className="Comment">
-	      Comment content
-      </p>
-    );
-  }
-}
-
-export default Comment;
+// create your comment component here
