93a23cc440a3629656994897753fa321295179b7
Alex White <awhit012@gmail.com>
2018-01-31 11:52:42 -0600

    puts code back to starter for back end

--- back-end/models/comment.js
+++ back-end/models/comment.js
@@ -1,11 +1,1 @@
-var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
-
-var CommentSchema = new Schema({
-	content: String,
-	votes: Number
-});
-
-var Comment = mongoose.model('Comment', CommentSchema);
-
-module.exports = Comment;
+// Add your comment model here
--- back-end/models/textPost.js
+++ back-end/models/textPost.js
@@ -1,15 +1,1 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    Comment = require('./comment');
-
-var TextPostSchema = new Schema({
-	title: String, 
-	content: String, 
-	thumbnail_image_url: String, 
-	votes: Number,
-	comments: [Comment.Schema] 
-});
-
-var TextPost = mongoose.model('TextPost', TextPostSchema);
-
-module.exports = TextPost;
+// Add your textPost model here
